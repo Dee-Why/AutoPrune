539187eec38a1af0167c6b49db9b89ab58dc0eb7
Dee-Why <by_liumingchao@pku.edu.cn>
2022-04-13 02:20:44 +0800

    evolve_linear has the full pipline

---
+++ torch_pruning/experiment.py
@@ -0,0 +1,84 @@
+import torch
+import torchvision
+import time
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def load_flatten_fashion_mnist(batch_size, resize=None, root='~/Datasets'):
+    """Download the fashion mnist dataset and then load into memory."""
+    trans = []
+    if resize:
+        trans.append(torchvision.transforms.Resize(size=resize))
+    trans.append(torchvision.transforms.ToTensor())
+    trans.append(torchvision.transforms.Lambda(lambda x: torch.flatten(x)))
+    transform = torchvision.transforms.Compose(trans)
+
+    mnist_train = torchvision.datasets.FashionMNIST(root=root, train=True, download=True, transform=transform)
+    mnist_test = torchvision.datasets.FashionMNIST(root=root, train=False, download=True, transform=transform)
+
+    train_iter = torch.utils.data.DataLoader(mnist_train, batch_size=batch_size, shuffle=True, num_workers=0)
+    test_iter = torch.utils.data.DataLoader(mnist_test, batch_size=batch_size, shuffle=False, num_workers=0)
+
+    return train_iter, test_iter
+
+
+def evaluate_accuracy(data_iter, net, device=None):
+    if device is None and isinstance(net, torch.nn.Module):
+        # 如果没指定device就使用net的device
+        device = list(net.parameters())[0].device
+    acc_sum, n = 0.0, 0
+    with torch.no_grad():
+        for X, y in data_iter:
+            if isinstance(net, torch.nn.Module):
+                net.eval()  # 评估模式, 这会关闭dropout
+                acc_sum += (net(X.to(device)).argmax(dim=1) == y.to(device)).float().sum().cpu().item()
+                net.train()  # 改回训练模式
+            else:  # 自定义的模型, 3.13节之后不会用到, 不考虑GPU
+                if ('is_training' in net.__code__.co_varnames):  # 如果有is_training这个参数
+                    # 将is_training设置成False
+                    acc_sum += (net(X, is_training=False).argmax(dim=1) == y).float().sum().item()
+                else:
+                    acc_sum += (net(X).argmax(dim=1) == y).float().sum().item()
+            n += y.shape[0]
+    return acc_sum / n
+
+
+def train_ch5(net, train_iter, test_iter, batch_size, optimizer, device, num_epochs):
+    incumbent_test_accuracy = 0
+    incumbent_epoch = 0
+    net = net.to(device)
+    print("training on ", device)
+    loss = torch.nn.CrossEntropyLoss()
+    for epoch in range(num_epochs):
+        train_l_sum, train_acc_sum, n, batch_count, start = 0.0, 0.0, 0, 0, time.time()
+        for X, y in train_iter:
+            X = X.to(device)
+            y = y.to(device)
+            y_hat = net(X)
+            l = loss(y_hat, y)
+            optimizer.zero_grad()
+            l.backward()
+            optimizer.step()
+            train_l_sum += l.cpu().item()
+            train_acc_sum += (y_hat.argmax(dim=1) == y).sum().cpu().item()
+            n += y.shape[0]
+            batch_count += 1
+        test_acc = evaluate_accuracy(test_iter, net)
+        if incumbent_test_accuracy < test_acc:
+            incumbent_test_accuracy = test_acc
+            incumbent_epoch = epoch
+        print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f, time %.1f sec'
+              % (epoch + 1, train_l_sum / batch_count, train_acc_sum / n, test_acc, time.time() - start))
+    return {'incumbent_epoch': incumbent_epoch, 'incumbent_test_accuracy': incumbent_test_accuracy}
+
+
+
+
+def fast_train(net, num_epochs):
+    batch_size = 128
+    train_iter, test_iter = load_flatten_fashion_mnist(batch_size,
+                                                       resize=15)  # 如出现“out of memory”的报错信息，可减小batch_size或resize
+    lr = 0.001
+    optimizer = torch.optim.Adam(net.parameters(), lr=lr)
+    res = train_ch5(net, train_iter, test_iter, batch_size, optimizer, device, num_epochs)
+    net.performance = res['incumbent_test_accuracy']
