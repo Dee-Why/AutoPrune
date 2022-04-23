32a0a9efb792e5e886826e527225336f4487c658
Dee-Why <by_liumingchao@pku.edu.cn>
2022-04-23 14:14:46 +0800

    Create quick_openbox.py

---
+++ examples/quick_openbox.py
@@ -0,0 +1,20 @@
+import numpy as np
+from openbox import Optimizer, sp
+
+# Define Search Space
+space = sp.Space()
+x1 = sp.Real("x1", -5, 10, default_value=0)
+x2 = sp.Real("x2", 0, 15, default_value=0)
+space.add_variables([x1, x2])
+
+# Define Objective Function
+def branin(config):
+    x1, x2 = config['x1'], config['x2']
+    y = (x2-5.1/(4*np.pi**2)*x1**2+5/np.pi*x1-6)**2+10*(1-1/(8*np.pi))*np.cos(x1)+10
+    return y
+
+# Run
+if __name__ == '__main__':
+    opt = Optimizer(branin, space, max_runs=50, task_id='quick_start')
+    history = opt.run()
+    print(history)
