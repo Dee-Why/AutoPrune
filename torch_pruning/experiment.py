2dacb485d722894ab55365dce97d0047f70bcb73
Dee-Why <by_liumingchao@pku.edu.cn>
2022-05-10 23:45:48 +0800

    add cifar100 dataset and AlexNetCifar100 model

--- torch_pruning/experiment.py
+++ torch_pruning/experiment.py
@@ -1,6 +1,7 @@
 import torch
 import torchvision
 import torch.nn as nn
+import torch.nn.functional as F
 import time
 from tensorboardX import SummaryWriter
 
@@ -21,6 +22,25 @@ def load_data_fashion_mnist(batch_size, resize=None, root='~/Datasets', flatten=
 
     train_iter = torch.utils.data.DataLoader(mnist_train, batch_size=batch_size, shuffle=True, num_workers=0)
     test_iter = torch.utils.data.DataLoader(mnist_test, batch_size=batch_size, shuffle=False, num_workers=0)
+
+    return train_iter, test_iter
+
+
+def load_data_cifar100(batch_size, resize=None, root='~/Datasets', flatten=False):
+    "Download the cifar100 dataset and load to memory, origin size per pic (32,32,3)"
+    trans = []
+    if resize:
+        trans.append(torchvision.transforms.Resize(size=resize))
+    trans.append(torchvision.transforms.ToTensor())
+    if flatten:
+        trans.append(torchvision.transforms.Lambda(lambda x: torch.flatten(x)))
+    transform = torchvision.transforms.Compose(trans)
+
+    cifar100_train = torchvision.datasets.CIFAR100(root=root, train=True, download=True, transform=transform)
+    cifar100_test = torchvision.datasets.CIFAR100(root=root, train=False, download=True, transform=transform)
+
+    train_iter = torch.utils.data.DataLoader(cifar100_train, batch_size=batch_size, shuffle=True, num_workers=0)
+    test_iter = torch.utils.data.DataLoader(cifar100_test, batch_size=batch_size, shuffle=False, num_workers=0)
 
     return train_iter, test_iter
 
@@ -220,3 +240,58 @@ def fast_evaluate_le(net):
     res = evaluate_accuracy(test_iter, net)
     net.performance = res
     return res
+
+
+class AlexNetCifar(nn.Module):
+    def __init__(self):
+        super(AlexNetMnist, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(3, 96, 11, 4), # in_channels, out_channels, kernel_size, stride, padding
+            nn.ReLU(),
+            nn.MaxPool2d(3, 2), # kernel_size, stride
+            # 减小卷积窗口，使用填充为2来使得输入与输出的高和宽一致，且增大输出通道数
+            nn.Conv2d(96, 256, 5, 1, 2),
+            nn.ReLU(),
+            nn.MaxPool2d(3, 2),
+            # 连续3个卷积层，且使用更小的卷积窗口。除了最后的卷积层外，进一步增大了输出通道数。
+            # 前两个卷积层后不使用池化层来减小输入的高和宽
+            nn.Conv2d(256, 384, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(384, 384, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(384, 256, 3, 1, 1),
+            nn.ReLU(),
+            nn.MaxPool2d(3, 2)
+        )
+         # 这里全连接层的输出个数比LeNet中的大数倍。使用丢弃层来缓解过拟合
+        self.fc = nn.Sequential(
+            nn.Linear(256*5*5, 4096),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            # 输出层。由于这里使用Fashion-MNIST，所以用类别数为10，而非论文中的1000
+            nn.Linear(4096, 100),
+        )
+
+    def forward(self, img):
+        feature = self.conv(img)
+        output = self.fc(feature.view(img.shape[0], -1))
+        return output
+
+def fast_train_alex_cifar100(net, num_epochs):
+    batch_size = 128
+    train_iter, test_iter = load_data_cifar100(batch_size, resize=224, flatten=False)
+    lr = 0.001
+    optimizer = torch.optim.Adam(net.parameters(), lr=lr)
+    res = train_ch5(net, train_iter, test_iter, batch_size, optimizer, device, num_epochs)
+    net.performance = res['incumbent_test_accuracy']
+
+def fast_evaluate_alex_cifar100(net):
+    batch_size = 128
+    train_iter, test_iter = load_data_cifar100(batch_size, resize=224, flatten=False)
+    res = evaluate_accuracy(test_iter, net)
+    net.performance = res
+    return res
+
